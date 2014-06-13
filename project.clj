2ee9e59ba0b3dae397e4722ba2685a439b560ac5
Troy Hill <thill@snowfall.local>
2014-06-13 15:12:38 -0600

    [TH] update datomic version and add dev-resources

--- project.clj
+++ project.clj
@@ -2,6 +2,7 @@
   :description "Datomic utilities"
   :url "http://github.com/turbovote/datomic-toolbox"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [com.datomic/datomic-pro "0.9.4609"]
+                 [com.datomic/datomic-pro "0.9.4755"]
                  [turbovote.resource-config "0.1.0"]]
-  :profiles {:test {:resource-paths ["test-resources"]}})
+  :profiles {:dev {:resource-paths ["dev-resources"]}
+             :test {:resource-paths ["test-resources"]}})
