7e47aaa0a4c15555c66cabe0f0eea4502986d630
Troy Hill <thill@snowfall.local>
2014-08-06 11:17:42 -0600

    [TH CS] retrieve timestamps on entity

--- src/turbovote/datomic_toolbox.clj
+++ src/turbovote/datomic_toolbox.clj
@@ -159,16 +159,18 @@
   "Returns a hash with keys :created-at, :updated-at, and :timestamps,
    the latter being all the timestamps of transactions affecting the
    entity in ascending order.
-   Pass in the database and the entity id."
-  [db id]
-  (let [tx-instants (->> (d/q '[:find ?txInstant
-                                :in $ ?e
-                                :where
-                                [?e _ _ ?tx]
-                                [?tx :db/txInstant ?txInstant]]
-                              (d/history db) id)
-                         (map first)
-                         sort)]
-    {:created-at (first tx-instants)
-     :updated-at (last tx-instants)
-     :timestamps tx-instants}))
+   Pass in the entity or a database and the entity id."
+  ([entity]
+     (timestamps (d/entity-db entity) (:db/id entity)))
+  ([db id]
+      (let [tx-instants (->> (d/q '[:find ?txInstant
+                                    :in $ ?e
+                                    :where
+                                    [?e _ _ ?tx]
+                                    [?tx :db/txInstant ?txInstant]]
+                                  (d/history db) id)
+                             (map first)
+                             sort)]
+        {:created-at (first tx-instants)
+         :updated-at (last tx-instants)
+         :timestamps tx-instants})))
