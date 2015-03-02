64d005fc92d582de9c33d4dc5d214f31ddbc1d0f
Wes Morgan <cap10morgan@gmail.com>
2015-03-02 10:27:40 -0700

    update Datomic to 0.9.5130

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "Datomic utilities"
   :url "http://github.com/turbovote/datomic-toolbox"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [com.datomic/datomic-pro "0.9.4755"]
+                 [com.datomic/datomic-pro "0.9.5130"]
                  [turbovote.resource-config "0.1.0"]]
   :profiles {:dev {:resource-paths ["dev-resources"]}
              :test {:resource-paths ["test-resources"]}})
