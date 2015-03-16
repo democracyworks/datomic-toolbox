3e4a0f646f53848c75dc1bcd23f6c44bc5ad6163
Wes Morgan <cap10morgan@gmail.com>
2015-03-15 18:47:14 -0600

    [WM] update deps, change groupId to democracyworks, release 0.2.5

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject turbovote.datomic-toolbox "0.2.5-SNAPSHOT"
+(defproject democracyworks.datomic-toolbox "0.2.5"
   :description "Datomic utilities"
-  :url "http://github.com/turbovote/datomic-toolbox"
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :url "http://github.com/democracyworks/datomic-toolbox"
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [com.datomic/datomic-pro "0.9.5130"]
-                 [turbovote.resource-config "0.1.0"]]
+                 [turbovote.resource-config "0.1.4"]]
   :profiles {:dev {:resource-paths ["dev-resources"]}
              :test {:resource-paths ["test-resources"]}})
