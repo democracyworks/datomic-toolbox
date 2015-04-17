31e26a7aa3b50fb4f04965115901bdc52e53a163
Wes Morgan <cap10morgan@gmail.com>
2015-04-17 16:39:30 -0600

    [WM] release v1.0.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject democracyworks/datomic-toolbox "1.0.0-SNAPSHOT"
+(defproject democracyworks/datomic-toolbox "1.0.0"
   :description "Datomic utilities"
   :url "http://github.com/democracyworks/datomic-toolbox"
   :license {:name "Eclipse Public License"
