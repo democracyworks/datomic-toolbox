01494835f70c393667cc97517fc18b9132a6a0c6
Wes Morgan <cap10morgan@gmail.com>
2015-07-15 16:15:35 -0600

    [WM] update clojure to 1.7.0

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://github.com/democracyworks/datomic-toolbox"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [com.datomic/datomic-pro "0.9.5153"]
                  [turbovote.resource-config "0.2.0"]]
   :repositories {"my.datomic.com" {:url "https://my.datomic.com/repo"
