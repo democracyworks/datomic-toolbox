2cf67cd54cf8d355845bdbce5f3d34a92ea5e126
Chris Shea <cmshea@gmail.com>
2015-07-28 11:04:17 -0400

    Prep 1.1.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject democracyworks/datomic-toolbox "1.1.0"
+(defproject democracyworks/datomic-toolbox "1.1.1-SNAPSHOT"
   :description "Datomic utilities"
   :url "http://github.com/democracyworks/datomic-toolbox"
   :license {:name "Eclipse Public License"
