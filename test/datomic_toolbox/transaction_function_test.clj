f9519d48cb4babdf0775f2e427431f62fda9a277
Daniel Glauser <danglauser@gmail.com>
2015-09-02 16:16:36 -0600

    [EN DG] Many cardinality

--- test/datomic_toolbox/transaction_function_test.clj
+++ test/datomic_toolbox/transaction_function_test.clj
@@ -15,102 +15,143 @@
     (db/run-migrations)
     (test)))
 
+(defn add-relation!
+  "Given a relationships returns an eid, relationship, and uuid for
+  the newly transacted entity."
+  [rel constr]
+  (let [tempid (db/tempid)
+        value (constr)
+        tx-data [{:db/id tempid
+                  rel value}]
+        {:keys [db-after tempids]} @(d/transact (db/connection) tx-data)
+        eid (d/resolve-tempid db-after tempids tempid)]
+    [eid rel value]))
+
+(defn rand-uuid []
+  (java.util.UUID/randomUUID))
+
+(defn rand-uuids []
+  (set [(rand-uuid) (rand-uuid) (rand-uuid)]))
+
 ;; one value
 
 (deftest one-value-test
   (testing "setting one-value anew"
     (let [tempid (db/tempid)
           uuid (java.util.UUID/randomUUID)
+          tx-data [[:transact tempid :transaction-test/one-value nil uuid]]
+          {:keys [db-after tempids]} @(d/transact (db/connection) tx-data)
+          eid (d/resolve-tempid db-after tempids tempid)
+          entity (d/entity db-after eid)]
+      (is (= uuid (:transaction-test/one-value entity)))))
 
-          {:keys [db-after tempids]}
-          @(d/transact (db/connection)
-                       [[:transact tempid
-                         :transaction-test/one-value nil uuid]])
-
-          eid (d/resolve-tempid db-after tempids tempid)]
-      (assert eid)
-      (let [response (d/q '{:find [?uuid]
-                            :in [$ ?eid]
-                            :where [[?eid :transaction-test/one-value ?uuid]]}
-                          db-after
-                          eid)]
-        (is (= uuid (first (first response)))))))
   (testing "setting existing one-value"
-    (let [tempid (db/tempid)
-          uuid (java.util.UUID/randomUUID)
-
-          {:keys [db-after tempids]}
-          @(d/transact (db/connection)
-                       [[:db/add tempid
-                         :transaction-test/one-value uuid]])
-
-          eid (d/resolve-tempid db-after tempids tempid)
+    (let [[eid rel uuid] (add-relation! :transaction-test/one-value rand-uuid)
           uuid2 (java.util.UUID/randomUUID)
+          tx-data [[:transact eid :transaction-test/one-value uuid uuid2]]
+          {:keys [db-after]} @(d/transact (db/connection) tx-data)
+          entity (d/entity db-after eid)]
+      (is (= uuid2 (:transaction-test/one-value entity)))))
 
-          {:keys [db-after]}
-          @(d/transact (db/connection)
-                       [[:transact eid :transaction-test/one-value uuid uuid2]])
+  (testing "setting existing one-value over the same value"
+    (let [[eid rel uuid] (add-relation! :transaction-test/one-value rand-uuid)
+          tx-data [[:transact eid :transaction-test/one-value uuid uuid]]
+          {:keys [db-after]} @(d/transact (db/connection) tx-data)
+          entity (d/entity db-after eid)]
+      (is (= uuid (:transaction-test/one-value entity)))))
 
-          response (d/q '{:find [?uuid]
-                          :in [$ ?eid]
-                          :where [[?eid :transaction-test/one-value ?uuid]]}
-                        db-after
-                        eid)]
-      (is (= uuid2 (first (first response))))))
   (testing "setting existing one-value with incorrect existing"
-    (let [tempid (db/tempid)
-          uuid (java.util.UUID/randomUUID)
-
-          {:keys [db-after tempids]}
-          @(d/transact (db/connection)
-                       [[:db/add tempid
-                         :transaction-test/one-value uuid]])
-
-          wrong-uuid (java.util.UUID/randomUUID)
-          eid (d/resolve-tempid db-after tempids tempid)
-          uuid2 (java.util.UUID/randomUUID)]
-
+    (let [[eid rel uuid] (add-relation! :transaction-test/one-value rand-uuid)
+          wrong (java.util.UUID/randomUUID)
+          uuid2 (java.util.UUID/randomUUID)
+          tx-data [[:transact eid :transaction-test/one-value wrong uuid2]]]
       (is (thrown? Throwable
-                   @(d/transact (db/connection)
-                                [[:transact eid :transaction-test/one-value wrong-uuid uuid2]])))
-      (let [response (d/q '{:find [?uuid]
-                            :in [$ ?eid]
-                            :where [[?eid :transaction-test/one-value ?uuid]]}
-                          (db/db)
-                          eid)]
-        (is (= uuid (first (first response)))))))
+                   @(d/transact (db/connection) tx-data)))
+      (let [entity (d/entity (db/db) eid)]
+        (is (= uuid (:transaction-test/one-value entity))))))
+
   (testing "setting existing one-value to nil"
-    (let [tempid (db/tempid)
-          uuid (java.util.UUID/randomUUID)
+    (let [[eid rel uuid] (add-relation! :transaction-test/one-value rand-uuid)
+          tx-data [[:transact eid :transaction-test/one-value uuid nil]]
+          {:keys [db-after]} @(d/transact (db/connection) tx-data)
+          entity (d/entity db-after eid)]
+      (is (nil? (:transaction-test/one-value entity)))))
 
-          {:keys [db-after tempids]}
-          @(d/transact (db/connection)
-                       [[:db/add tempid
-                         :transaction-test/one-value uuid]])
-
-          eid (d/resolve-tempid db-after tempids tempid)
-
-          {:keys [db-after]}
-          @(d/transact (db/connection)
-                       [[:transact eid :transaction-test/one-value uuid nil]])
-          response (d/q '{:find [?uuid]
-                          :in [$ ?eid]
-                          :where [[?eid :transaction-test/one-value ?uuid]]}
-                        (db/db)
-                        eid)]
-      (is (empty? response)))))
+  (testing "setting non-existant one-value to nil"
+    (let [[eid rel uuid] (add-relation! :transaction-test/many-value rand-uuids)
+          tx-data [[:transact eid :transaction-test/one-value nil nil]]
+          {:keys [db-after]} @(d/transact (db/connection) tx-data)
+          entity (d/entity db-after eid)]
+      (is (nil? (:transaction-test/one-value entity))))))
 
 ;; many value
-;; setting anew
-;; setting existing with correct existing value
-;; setting existing with incorrect existing value
 
-;; one ref
-;; setting anew
-;; setting existing with correct existing value
-;; setting existing with incorrect existing value
+(deftest many-value-test
+  (testing "setting many-value anew"
+    (let [tempid (db/tempid)
+          uuids (set [(java.util.UUID/randomUUID) (java.util.UUID/randomUUID)])
+          tx-data [[:transact tempid :transaction-test/many-value nil uuids]]
+          {:keys [db-after tempids]} @(d/transact (db/connection) tx-data)
+          eid (d/resolve-tempid db-after tempids tempid)
+          entity (d/entity db-after eid)]
+      (is (= uuids (:transaction-test/many-value entity)))))
 
-;; many ref
-;; setting anew
-;; setting existing with correct existing value
-;; setting existing with incorrect existing value
+  (testing "setting existing many-value"
+    (let [[eid rel uuids] (add-relation! :transaction-test/many-value rand-uuids)
+          uuids2 (rand-uuids)
+          tx-data [[:transact eid :transaction-test/many-value uuids uuids2]]
+          {:keys [db-after]} @(d/transact (db/connection) tx-data)
+          entity (d/entity db-after eid)]
+      (is (= uuids2 (:transaction-test/many-value entity)))))
+
+  (testing "setting existing many-value over the same value"
+    (let [[eid rel uuids] (add-relation! :transaction-test/many-value rand-uuids)
+          tx-data [[:transact eid :transaction-test/many-value uuids uuids]]
+          {:keys [db-after]} @(d/transact (db/connection) tx-data)
+          entity (d/entity db-after eid)]
+      (is (= uuids (:transaction-test/many-value entity)))))
+  (testing "setting existing many-value with incorrect existing"
+    (let [[eid rel uuids] (add-relation! :transaction-test/many-value rand-uuids)
+          wrong (rand-uuids)
+          uuids2 (rand-uuids)
+          tx-data [[:transact eid :transaction-test/many-value wrong uuids2]]]
+      (is (thrown? Throwable
+                   @(d/transact (db/connection) tx-data)))
+      (let [entity (d/entity (db/db) eid)]
+        (is (= uuids (:transaction-test/many-value entity))))))
+  (testing "setting existing many-value to nil"
+    (let [[eid rel uuids] (add-relation! :transaction-test/many-value rand-uuids)
+          tx-data [[:transact eid :transaction-test/many-value uuids nil]]
+          {:keys [db-after]} @(d/transact (db/connection) tx-data)
+          entity (d/entity db-after eid)]
+      (is (nil? (:transaction-test/many-value entity)))))
+  (testing "setting non-existant many-value to nil"
+    (let [[eid rel uuid] (add-relation! :transaction-test/one-value rand-uuid)
+          tx-data [[:transact eid :transaction-test/many-value nil nil]]
+          {:keys [db-after]} @(d/transact (db/connection) tx-data)
+          entity (d/entity db-after eid)]
+      (is (nil? (:transaction-test/many-value entity)))))
+  
+
+
+
+
+
+
+
+
+  ;; setting anew
+  ;; setting existing with correct existing value
+  ;; setting existing with incorrect existing value
+
+  ;; one ref
+  ;; setting anew
+  ;; setting existing with correct existing value
+  ;; setting existing with incorrect existing value
+
+  ;; many ref
+  ;; setting anew
+  ;; setting existing with correct existing value
+  ;; setting existing with incorrect existing value
+  
+  )
