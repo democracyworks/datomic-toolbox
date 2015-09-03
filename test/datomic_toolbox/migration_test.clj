c81e61e160b9861aede051def5841c04f543e1da
Eric Normand <ericwnormand@gmail.com>
2015-09-03 16:53:42 -0500

    [EN DG] Improve migration tests.

--- test/datomic_toolbox/migration_test.clj
+++ test/datomic_toolbox/migration_test.clj
@@ -15,34 +15,35 @@
 
 (use-fixtures :each migration-ready-db)
 
+(def known-schema-files
+  ["001-schema.edn"
+   "002-schema.edn"
+   "003-unique-id.edn"
+   "004-transaction-tests.edn"])
+
 (deftest schema-files-test
   (testing "finds schema files"
-    (is (= 3 (count (schema-files))))
-    (let [filenames (->> (schema-files)
-                         (map #(.getName %))
-                         set)]
-      (is (filenames "001-schema.edn"))
-      (is (filenames "002-schema.edn"))
-      (is (filenames "003-unique-id.edn"))
-      (is (= "001-schema.edn" (-> (schema-files) first (.getName)))))))
+    (is (= known-schema-files (map #(.getName %) (schema-files))))))
 
 (deftest applied-migrations-test
-  (testing "no applied migratons"
-    (is (empty? (applied-migrations))))
-  (testing "with an applied migration"
-    (run-migration (first (schema-files)))
-    (is ((applied-migrations) "001-schema.edn")))
-  (testing "with all migrations applied"
-    (run-migrations)
-    (is (= 3 (count (applied-migrations))))))
+  (let [migration-count (count (schema-files))]
+   (testing "no applied migratons"
+     (is (empty? (applied-migrations))))
+   (testing "with an applied migration"
+     (run-migration (first (schema-files)))
+     (is ((applied-migrations) "001-schema.edn")))
+   (testing "with all migrations applied"
+     (run-migrations)
+     (is (= migration-count (count (applied-migrations)))))))
 
 (deftest unapplied-migrations-test
-  (testing "no applied migrations"
-    (is (= 3 (count (unapplied-migrations)))))
-  (testing "with an applied migration"
-    (run-migration (first (schema-files)))
-    (is (= 2 (count (unapplied-migrations))))
-    (is (= "002-schema.edn" (-> (unapplied-migrations) first (.getName)))))
-  (testing "with all migrations applied"
-    (run-migrations)
-    (is (empty? (unapplied-migrations)))))
+  (let [migration-count (count (schema-files))]
+   (testing "no applied migrations"
+     (is (= migration-count (count (unapplied-migrations)))))
+   (testing "with an applied migration"
+     (run-migration (first (schema-files)))
+     (is (= (dec migration-count) (count (unapplied-migrations))))
+     (is (= "002-schema.edn" (-> (unapplied-migrations) first (.getName)))))
+   (testing "with all migrations applied"
+     (run-migrations)
+     (is (empty? (unapplied-migrations))))))
