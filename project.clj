c407fc6cb2a44e69e4c1214bbb77d1ff74717b2b
Eric Normand <ericwnormand@gmail.com>
2015-09-16 10:13:39 -0500

    Version 1.1.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject democracyworks/datomic-toolbox "1.1.1-transact-SNAPSHOT"
+(defproject democracyworks/datomic-toolbox "1.1.1"
   :description "Datomic utilities"
   :url "http://github.com/democracyworks/datomic-toolbox"
   :license {:name "Eclipse Public License"
