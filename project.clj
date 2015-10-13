16882cd07260b79e25ea9fb128d9c6c5148d9e35
Wes Morgan <cap10morgan@gmail.com>
2015-10-13 12:56:00 -0600

    [WM] bump version to 2.0.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject democracyworks/datomic-toolbox "1.1.1-transact-SNAPSHOT"
+(defproject democracyworks/datomic-toolbox "2.0.0-SNAPSHOT"
   :description "Datomic utilities"
   :url "http://github.com/democracyworks/datomic-toolbox"
   :license {:name "Eclipse Public License"
