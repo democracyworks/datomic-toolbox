7fd19da97e5217afccc3289ebca821d0dbd4b851
Wes Morgan <wesmorgan@icloud.com>
2016-08-03 09:34:58 -0600

    Update deps

--- project.clj
+++ project.clj
@@ -3,8 +3,9 @@
   :url "http://github.com/democracyworks/datomic-toolbox"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [com.datomic/datomic-pro "0.9.5153"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [com.datomic/datomic-pro "0.9.5385"]
+                 [com.amazonaws/aws-java-sdk-dynamodb "1.11.6"]]
   :repositories {"my.datomic.com" {:url "https://my.datomic.com/repo"
                                    :username [:gpg :env]
                                    :password [:gpg :env]}}
