b4d4066eaf83a75fd4a905e4023ec9b9fdd50298
Wes Morgan <wesmorgan@icloud.com>
2016-09-05 09:20:11 -0600

    Upgrade datomic to 0.9.5394

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [com.datomic/datomic-pro "0.9.5385"]
+                 [com.datomic/datomic-pro "0.9.5394"]
                  [com.amazonaws/aws-java-sdk-dynamodb "1.11.6"]]
   :repositories {"my.datomic.com" {:url "https://my.datomic.com/repo"
                                    :username [:gpg :env]
