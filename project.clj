e0748d411ff2ab1dc931df97f91e015762df7adc
Wes Morgan <wesmorgan@icloud.com>
2016-11-10 10:40:23 -0700

    Version 2.0.4-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject democracyworks/datomic-toolbox "2.0.3"
+(defproject democracyworks/datomic-toolbox "2.0.4-SNAPSHOT"
   :description "Datomic utilities"
   :url "http://github.com/democracyworks/datomic-toolbox"
   :license {:name "Eclipse Public License"
